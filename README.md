524045c2f31477aab7798ea131def2d7e2c51d1e
Chinmay Senapathi <cksenapathi@utexas.edu>
2022-01-05 19:31:52 -0600

    wrote basic code to get quad to stand up; currently does a barrel roll on rising; looking to change controller to effort based rather than position based

---
+++ README.md
@@ -0,0 +1,14 @@
+## bittle-walker
+ROS package for Gazebo simulation for a bittle robot and gait generation in the future
+
+# Current State:
+- Working in gazebo
+- Sliding behavior when trying to get up on legs
+- need to fix contact point friction values
+- joint control gui is available, but does not publish data to gazebo
+
+# Dependencies:
+- ROS Noetic on Ubuntu 20.04
+- Not sure which packages are actually being used lmao
+- just clone it and try to run `roslaunch quad_walker gazebo.launch` and see what crashes
+- 
---
+++ src/quad_walk.cpp
@@ -0,0 +1,37 @@
+#include <ros/ros.h>
+#include <std_msgs/Float64MultiArray.h>
+#include <std_msgs/MultiArrayDimension.h>
+//#include <tf2_msgs/TFMessage>
+#include <vector>
+
+// Right side angles need to be negative, left side will be positive
+// angle order goes {lbk, lbs, lfk, lfs, rbk, rbs, rfk, rfs}
+
+const double PI = 3.141592;
+
+int main(int argc, char** argv){
+
+  ros::init(argc, argv, "controller");
+
+  ros::NodeHandle n;
+  ros::Rate rate(50);
+
+  ros::Publisher joints_pub = n.advertise<std_msgs::Float64MultiArray>("/bittle/joints_controller/command",1000);
+
+  std_msgs::Float64MultiArray joint_array;
+
+  joint_array.data = std::vector<double>({0.3, 0.5, 0.3, 0.5, -0.3, -0.5, -0.3, -0.5});
+  std_msgs::MultiArrayDimension dim;
+  dim.label = "dim1";
+  dim.size = 9;
+  dim.stride = 1;
+  joint_array.layout.data_offset = 0;
+  joint_array.layout.dim = std::vector<std_msgs::MultiArrayDimension>({dim});
+
+  while (ros::ok()){
+    joints_pub.publish(joint_array);
+    ros::spinOnce();
+    rate.sleep();
+  }
+  return 0;
+}
